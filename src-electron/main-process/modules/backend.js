9dc3d7b1271b18a5818fa0ea570c3d7a0f0aafd4
evolution-project <60467816+evolution-project@users.noreply.github.com>
2021-06-24 19:08:13 +0300

    Update backend.js

--- src-electron/main-process/modules/backend.js
+++ src-electron/main-process/modules/backend.js
@@ -1,4 +1,3 @@
-
 import { Daemon } from "./daemon"
 import { WalletRPC } from "./wallet-rpc"
 import { Market } from "./market"
@@ -171,7 +170,7 @@ export class Backend {
                     hostname: "api.coingecko.com",
                     port: 443,
                     coin: "evolution",
-                    endpoint: "/api/v3/coins/evolution/tickers"
+                    endpoint: "/api/v3/coins/evox/tickers"
                 }
             },
 
@@ -324,187 +323,187 @@ export class Backend {
                   break
 
                   case "open_url":
-                    require("electron").shell.openExternal(params.url)
-                    break
-        
-                case "save_png":
-                    let filename = dialog.showSaveDialog(this.mainWindow, {
-                        title: "Save " + params.type,
-                        filters: [{ name: "PNG", extensions: ["png"] }],
-                        defaultPath: os.homedir()
-                    })
-                    if (filename) {
-                        let base64Data = params.img.replace(/^data:image\/png;base64,/, "")
-                        let binaryData = Buffer.from(base64Data).toString("binary")
-                        fs.writeFile(filename, binaryData, "binary", (err) => {
-                            if (err) { this.send("show_notification", { type: "negative", message: "Error saving " + params.type, timeout: 2000 }) } else { this.send("show_notification", { message: params.type + " saved to " + filename, timeout: 2000 }) }
-                        })
-                    }
-                    break
-        
-                default:
+            require("electron").shell.openExternal(params.url)
+            break
+
+        case "save_png":
+            let filename = dialog.showSaveDialog(this.mainWindow, {
+                title: "Save " + params.type,
+                filters: [{ name: "PNG", extensions: ["png"] }],
+                defaultPath: os.homedir()
+            })
+            if (filename) {
+                let base64Data = params.img.replace(/^data:image\/png;base64,/, "")
+                let binaryData = Buffer.from(base64Data).toString("binary")
+                fs.writeFile(filename, binaryData, "binary", (err) => {
+                    if (err) { this.send("show_notification", { type: "negative", message: "Error saving " + params.type, timeout: 2000 }) } else { this.send("show_notification", { message: params.type + " saved to " + filename, timeout: 2000 }) }
+                })
+            }
+            break
+
+        default:
+        }
+    }
+
+    startup () {
+        this.send("initialize")
+        fs.readFile(this.config_file, "utf8", (err, data) => {
+            if (err) {
+                this.send("set_app_data", {
+                    status: {
+                        code: -1 // Config not found
+                    },
+                    config: this.config_data,
+                    pending_config: this.config_data,
+                    remotes: this.remotes,
+                    defaults: this.defaults
+                })
+                return
+            }
+
+            let disk_config_data = {}
+            try {
+                disk_config_data = JSON.parse(data)
+            } catch (e) {
+            }
+
+            // semi-shallow object merge
+            Object.keys(disk_config_data).map(key => {
+                if (!this.config_data.hasOwnProperty(key)) { this.config_data[key] = {} }
+                this.config_data[key] = Object.assign(this.config_data[key], disk_config_data[key])
+            })
+
+            // If not Windows or Mac OS, and minimize to tray preference not set, force to false
+            // Else if is Windows or Mac OS, and minimize to tray preference not set, prevent minimize
+            if (this.config_data.preference.minimize_to_tray === null) {
+                if (os.platform() !== "win32" && os.platform() !== "darwin") {
+                    this.config_data.preference.minimize_to_tray = false
+                } else {
+                    this.mainWindow.setMinimizable(false)
                 }
             }
-        
-            startup () {
-                this.send("initialize")
-                fs.readFile(this.config_file, "utf8", (err, data) => {
-                    if (err) {
-                        this.send("set_app_data", {
-                            status: {
-                                code: -1 // Config not found
-                            },
-                            config: this.config_data,
-                            pending_config: this.config_data,
-                            remotes: this.remotes,
-                            defaults: this.defaults
+
+            // here we may want to check if config data is valid, if not also send code -1
+            // i.e. check ports are integers and > 1024, check that data dir path exists, etc
+
+            // Filter out http:// from remote_host (remote daemon address)
+            if (this.config_data.daemon.remote_host.indexOf("//") !== -1) {
+                let remote_host = this.config_data.daemon.remote_host
+                remote_host = this.config_data.daemon.remote_host.split("//")
+                remote_host.shift()
+                remote_host = remote_host.join("//")
+                this.config_data.daemon.remote_host = remote_host
+            }
+
+            // save config file back to file, so updated options are stored on disk
+            fs.writeFileSync(this.config_file, JSON.stringify(this.config_data, null, 4), "utf8")
+
+            // get network interfaces for UI
+            const interfaces = os.networkInterfaces()
+            let network_interfaces = [{
+                value: "0.0.0.0",
+                label: "All interfaces - 0.0.0.0"
+            }]
+            for (let k in interfaces) {
+                for (let k2 in interfaces[k]) {
+                    const address = interfaces[k][k2]
+                    if (address.family === "IPv4" && address.internal) {
+                        network_interfaces.push({
+                            value: address.address,
+                            label: `Local machine only - ${address.address}`
                         })
-                        return
-                    }
-        
-                    let disk_config_data = {}
-                    try {
-                        disk_config_data = JSON.parse(data)
-                    } catch (e) {
-                    }
-        
-                    // semi-shallow object merge
-                    Object.keys(disk_config_data).map(key => {
-                        if (!this.config_data.hasOwnProperty(key)) { this.config_data[key] = {} }
-                        this.config_data[key] = Object.assign(this.config_data[key], disk_config_data[key])
-                    })
-        
-                    // If not Windows or Mac OS, and minimize to tray preference not set, force to false
-                    // Else if is Windows or Mac OS, and minimize to tray preference not set, prevent minimize
-                    if (this.config_data.preference.minimize_to_tray === null) {
-                        if (os.platform() !== "win32" && os.platform() !== "darwin") {
-                            this.config_data.preference.minimize_to_tray = false
-                        } else {
-                            this.mainWindow.setMinimizable(false)
-                        }
-                    }
-        
-                    // here we may want to check if config data is valid, if not also send code -1
-                    // i.e. check ports are integers and > 1024, check that data dir path exists, etc
-        
-                    // Filter out http:// from remote_host (remote daemon address)
-                    if (this.config_data.daemon.remote_host.indexOf("//") !== -1) {
-                        let remote_host = this.config_data.daemon.remote_host
-                        remote_host = this.config_data.daemon.remote_host.split("//")
-                        remote_host.shift()
-                        remote_host = remote_host.join("//")
-                        this.config_data.daemon.remote_host = remote_host
-                    }
-        
-                    // save config file back to file, so updated options are stored on disk
-                    fs.writeFileSync(this.config_file, JSON.stringify(this.config_data, null, 4), "utf8")
-        
-                    // get network interfaces for UI
-                    const interfaces = os.networkInterfaces()
-                    let network_interfaces = [{
-                        value: "0.0.0.0",
-                        label: "All interfaces - 0.0.0.0"
-                    }]
-                    for (let k in interfaces) {
-                        for (let k2 in interfaces[k]) {
-                            const address = interfaces[k][k2]
-                            if (address.family === "IPv4" && address.internal) {
-                                network_interfaces.push({
-                                    value: address.address,
-                                    label: `Local machine only - ${address.address}`
-                                })
-                            } else if (address.family === "IPv4" && !address.internal) {
-                                network_interfaces.push({
-                                    value: address.address,
-                                    label: `Local network only - ${address.address}`
-                                })
-                            }
-                        }
-                    }
-        
-                    this.send("set_app_data", {
-                        config: this.config_data,
-                        pending_config: this.config_data,
-                        network_interfaces: network_interfaces,
-                        remotes: [...this.remotes]
-                    })
-        
-                    // Check to see if data dir exists, if not it may have been on network drive
-                    // if not exist, send back to config screen with message so user can select
-                    // new location
-                    if (!fs.existsSync(this.config_data.app.data_dir)) {
-                        this.send("show_notification", { type: "negative", message: "Error: data storge path not found", timeout: 2000 })
-                        this.send("set_app_data", {
-                            status: {
-                                code: -1 // Return to config screen
-                            }
+                    } else if (address.family === "IPv4" && !address.internal) {
+                        network_interfaces.push({
+                            value: address.address,
+                            label: `Local network only - ${address.address}`
                         })
-                        return
                     }
-        
-                    let lmdb_dir = path.join(this.config_data.app.data_dir, "lmdb02")
-                    let log_dir = path.join(this.config_data.app.data_dir, "logs")
-                    let wallet_dir = path.join(this.config_data.app.data_dir, "wallets")
-                    let gui_dir = path.join(this.config_data.app.data_dir, "gui")
-        
-                    if (this.config_data.app.testnet) {
-                        let testnet_dir = path.join(this.config_data.app.data_dir, "testnet")
-                        if (!fs.existsSync(testnet_dir)) { fs.mkdirSync(testnet_dir) }
-        
-                        lmdb_dir = path.join(testnet_dir, "lmdb02")
-                        log_dir = path.join(testnet_dir, "logs")
-                        wallet_dir = path.join(testnet_dir, "wallets")
-                        gui_dir = path.join(testnet_dir, "gui")
+                }
+            }
+
+            this.send("set_app_data", {
+                config: this.config_data,
+                pending_config: this.config_data,
+                network_interfaces: network_interfaces,
+                remotes: [...this.remotes]
+            })
+
+            // Check to see if data dir exists, if not it may have been on network drive
+            // if not exist, send back to config screen with message so user can select
+            // new location
+            if (!fs.existsSync(this.config_data.app.data_dir)) {
+                this.send("show_notification", { type: "negative", message: "Error: data storge path not found", timeout: 2000 })
+                this.send("set_app_data", {
+                    status: {
+                        code: -1 // Return to config screen
                     }
-        
-                    if (!fs.existsSync(lmdb_dir)) { fs.mkdirSync(lmdb_dir) }
-                    if (!fs.existsSync(log_dir)) { fs.mkdirSync(log_dir) }
-                    if (!fs.existsSync(wallet_dir)) { fs.mkdirSync(wallet_dir) }
-                    if (!fs.existsSync(gui_dir)) { fs.mkdirSync(gui_dir) }
-        
-                    // Check permissions
-                    try {
-                        fs.accessSync(this.config_dir, fs.constants.R_OK | fs.constants.W_OK)
-                        fs.accessSync(path.join(this.config_dir, "gui"), fs.constants.R_OK | fs.constants.W_OK)
-                        fs.accessSync(this.config_file, fs.constants.R_OK | fs.constants.W_OK)
-                        fs.accessSync(lmdb_dir, fs.constants.R_OK | fs.constants.W_OK)
-                        fs.accessSync(log_dir, fs.constants.R_OK | fs.constants.W_OK)
-                        fs.accessSync(wallet_dir, fs.constants.R_OK | fs.constants.W_OK)
-                    } catch (err) {
-                        this.send("show_notification", { type: "negative", message: "Error: data storge path not writable", timeout: 2000 })
-                        this.send("set_app_data", {
-                            status: {
-                                code: -1 // Return to config screen
-                            }
-                        })
-                        return
+                })
+                return
+            }
+
+            let lmdb_dir = path.join(this.config_data.app.data_dir, "lmdb02")
+            let log_dir = path.join(this.config_data.app.data_dir, "logs")
+            let wallet_dir = path.join(this.config_data.app.data_dir, "wallets")
+            let gui_dir = path.join(this.config_data.app.data_dir, "gui")
+
+            if (this.config_data.app.testnet) {
+                let testnet_dir = path.join(this.config_data.app.data_dir, "testnet")
+                if (!fs.existsSync(testnet_dir)) { fs.mkdirSync(testnet_dir) }
+
+                lmdb_dir = path.join(testnet_dir, "lmdb02")
+                log_dir = path.join(testnet_dir, "logs")
+                wallet_dir = path.join(testnet_dir, "wallets")
+                gui_dir = path.join(testnet_dir, "gui")
+            }
+
+            if (!fs.existsSync(lmdb_dir)) { fs.mkdirSync(lmdb_dir) }
+            if (!fs.existsSync(log_dir)) { fs.mkdirSync(log_dir) }
+            if (!fs.existsSync(wallet_dir)) { fs.mkdirSync(wallet_dir) }
+            if (!fs.existsSync(gui_dir)) { fs.mkdirSync(gui_dir) }
+
+            // Check permissions
+            try {
+                fs.accessSync(this.config_dir, fs.constants.R_OK | fs.constants.W_OK)
+                fs.accessSync(path.join(this.config_dir, "gui"), fs.constants.R_OK | fs.constants.W_OK)
+                fs.accessSync(this.config_file, fs.constants.R_OK | fs.constants.W_OK)
+                fs.accessSync(lmdb_dir, fs.constants.R_OK | fs.constants.W_OK)
+                fs.accessSync(log_dir, fs.constants.R_OK | fs.constants.W_OK)
+                fs.accessSync(wallet_dir, fs.constants.R_OK | fs.constants.W_OK)
+            } catch (err) {
+                this.send("show_notification", { type: "negative", message: "Error: data storge path not writable", timeout: 2000 })
+                this.send("set_app_data", {
+                    status: {
+                        code: -1 // Return to config screen
                     }
-                    this.daemon = new Daemon(this)
-                    this.walletd = new WalletRPC(this)
-                    if (this.config_data.pool.server.enabled)
-                        this.pool = new Pool(this)
-                    this.market = new Market(this)
-        
+                })
+                return
+            }
+            this.daemon = new Daemon(this)
+            this.walletd = new WalletRPC(this)
+            if (this.config_data.pool.server.enabled)
+                this.pool = new Pool(this)
+            this.market = new Market(this)
+
+            this.send("set_app_data", {
+                status: {
+                    code: 3 // Starting daemon
+                }
+            })
+
+            this.daemon.checkVersion().then((version) => {
+                if (version) {
                     this.send("set_app_data", {
                         status: {
-                            code: 3 // Starting daemon
+                            code: 4,
+                            message: version
                         }
                     })
-        
-                    this.daemon.checkVersion().then((version) => {
-                        if (version) {
-                            this.send("set_app_data", {
-                                status: {
-                                    code: 4,
-                                    message: version
-                                }
-                            })
-                        } else {
+                } else {
                     // daemon not found, probably removed by AV, set to remote node
                     this.config_data.daemon.type = "remote"
                     this.send("set_app_data", {
                         config: this.config_data,
-                        pending_config: this.config_data,
+                        pending_config: this.config_data
                     })
                     this.send("show_notification", {type: "warning", textColor: "black", message: "Warning: evolutiond not found, using remote node", timeout: 2000})
                 }
